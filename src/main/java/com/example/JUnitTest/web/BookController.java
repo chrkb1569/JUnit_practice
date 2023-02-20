8e5c482d08fd00c40de8fd22c3f91fa2fa38fa23
minsub <chrkb1569@naver.com>
2023-02-21 00:06:09 +0900

    feat : Add BookController File

--- src/main/java/com/example/JUnitTest/web/BookController.java
+++ src/main/java/com/example/JUnitTest/web/BookController.java
@@ -1,7 +1,44 @@
 package com.example.JUnitTest.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.JUnitTest.dto.BookEditDto;
+import com.example.JUnitTest.dto.BookSaveDto;
+import com.example.JUnitTest.service.BookService;
+import com.example.JUnitTest.web.dto.BookResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping("/books")
+    public ResponseEntity<List<BookResponseDto>> getBooks() {
+        return new ResponseEntity<>(bookService.getBooks(), HttpStatus.OK);
+    }
+
+    @GetMapping("/book/{id}")
+    public ResponseEntity<BookResponseDto> getBook(@PathVariable Long id) {
+        return new ResponseEntity<>(bookService.getBook(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Long> saveBook(@RequestBody BookSaveDto saveDto) {
+        return new ResponseEntity<>(bookService.saveBook(saveDto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/book/{id}")
+    public ResponseEntity<Long> editBook(@PathVariable Long id, @RequestBody BookEditDto editDto) {
+        return new ResponseEntity<>(bookService.editBook(editDto, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/book/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
 }
